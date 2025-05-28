c579d7ba7369adfd65a491d72ae040096629cd6c
caduzz <cadu0970@gmail.com>
2025-05-28 13:59:33 -0300

    banner mobile version

--- src/pages/home/climate/index.jsx
+++ src/pages/home/climate/index.jsx
@@ -2,7 +2,7 @@ import { Title } from "../../../components/title";
 
 export function Climate() {
   return (
-    <section id="clima" className="relative w-full overflow-hidden flex align-center justify-center md:p-10 md:mt-15 md:mb-22 m-y-4">
+    <section id="clima" className="relative w-full overflow-hidden flex align-center justify-center p-10 md:mt-15 md:mb-22 m-y-4">
       <div className="w-full max-w-[1250px] flex flex-col items-center justify-center">
         <Title>Conecte sua marca ao maior evento de Saneamento Rural e faça parte dessa transformação!</Title>
         <div className="w-full flex flex-col items-center justify-center">
