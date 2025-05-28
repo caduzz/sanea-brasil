d527afa1cc84e5f4dae54a4f006635bbecc835ea
caduzz <cadu0970@gmail.com>
2025-05-28 12:59:41 -0300

    banner mobile version

--- src/pages/home/banner/index.jsx
+++ src/pages/home/banner/index.jsx
@@ -1,6 +1,6 @@
 export function Banner() {
   return (
-    <section id="home" className="w-full md:h-[45vw] h-100 bg-gradient-to-b shadow from-[#FFFFFF] via-[#B0C8D4] to-[#BED6DA]">
+    <section id="home" className="w-full md:h-[45vw] h-80 bg-gradient-to-b shadow from-[#FFFFFF] via-[#B0C8D4] to-[#BED6DA]">
       <div className="h-full flex flex-col items-center justify-between gap-4 relative">
         <img src="./svg/svgbanner.svg" className="absolute h-full md:h-auto" />
         <img className="w-[50vw] mt-15" src="./svg/logo-sanea-pc.svg" />
