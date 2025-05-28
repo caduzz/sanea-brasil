e49160d93c2dcea584dd798dabc8175d41c3da3c
caduzz <cadu0970@gmail.com>
2025-05-28 19:04:08 -0300

    mobile version clima

--- src/pages/home/climate/index.jsx
+++ src/pages/home/climate/index.jsx
@@ -3,7 +3,7 @@ import { Title } from "../../../components/title";
 export function Climate() {
   return (
     <section id="clima" className="relative w-full overflow-hidden flex align-center justify-center p-10 md:mt-15 md:mb-22 m-y-4">
-      <div className="w-full max-w-[1250px] flex flex-col items-center justify-center">
+      <div className="w-full max-w-[1250px] flex flex-col items-center justify-center gap-10 md:gap-0">
         <Title>Conecte sua marca ao maior evento de Saneamento Rural e faça parte dessa transformação!</Title>
         <div className="w-full flex flex-col items-center justify-center">
           <img className="w-full" src="./svg/climate.svg" alt="Climate Illustration"/>
